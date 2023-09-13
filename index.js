1f145d2b0a7b0a2b79b043f6d3a471f554b312d0
rambriz91 <rambriz91@gmail.com>
2023-09-13 13:21:44 -0700

    added functions to render shapes, and GenerateSVG.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const shapes = require('./shapes/shapes');
+const generateSVG = require('./shapes/shapes.js');
 const inquirer = require('inquirer');
 
 inquirer
@@ -8,7 +8,7 @@ inquirer
             type: 'list',
             name: 'shape',
             message: `Please select a shape for your logo.`,
-            choices: ['Circle', 'Triangle', 'Rectangle', `Ellipse`]
+            choices: ['Circle', 'Triangle', 'Rectangle', 'Ellipse']
         },
         {
             type: 'input',
@@ -16,20 +16,30 @@ inquirer
             message: 'Please input a color or hexcode.'
         },
         {
-            input: 'input',
-            name: 'text',
+            type: 'input',
+            name: 'textInput',
             message: 'Please input your logo text, it must be between 1-3 characters in length.'
+        },
+        {
+            type: 'input',
+            name: 'fontColor',
+            message: 'Please input a font color or hexcode.'
+        },
+        {
+            type: 'input',
+            name: 'fontFamily',
+            message: 'Enter a font family for your logo text.'
         }
         
     ])
 .then((data) => {
-    console.log(data)
-    const {shape, color, text} = data;
-    if (text < 1 || text > 3) {
+    const {shape, color, textInput, fontColor, fontFamily} = data;
+    console.log(data);
+    console.log(generateSVG(data));
+    if (textInput < 1 || textInput > 3) {
         console.log('Logo text must include between 1-3 characters.')
         return;
     }
-    //generateMarkdown fxn accepts data and inputs into readme template and outputs completed readme to fs
 fs.writeFile('logo.svg', generateSVG(data), (err) =>
 err ? console.log(err) : console.log('Success!')
 );
--- shapes/shapes.js
+++ shapes/shapes.js
@@ -1,80 +1,119 @@
-// class SVG extends Shape{
-//     constructor(height, width) {
-//         this.heigth = height;
-//         this.width = width;
-//     }
-//     render() {
-//         return `<svg version="1.1"
-//         width="300" height="200"
-//         xmlns="http://www.w3.org/2000/svg"></svg>`
-//     }
-// };
-
 const { error } = require("console");
 const { throwError } = require("rxjs");
 
-generateSVG(data) {
+class SVG {
+    constructor() {
+        this.shape = '';
+        this.text = '';
 
-    
+    }
+    render() {
+        return `<svg version="1.1"
+         width="300" height="200"
+         xmlns="http://www.w3.org/2000/svg">
+         ${this.shape}
+         ${this.text}
+         </svg>`
+    };
+    setText(fontColor, fontFamily, textInput) {
+        this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${fontColor}" font-family='${fontFamily}>${textInput}</text>`
+        
+    };
+    setShape(shape) {
+        this.shape = shape.render();
+    };
+};
+
+function generateSVG(data) {
+   
+    const { shape, color, textInput, fontColor, fontFamily } = data;
+
+    const newLogo = new SVG();
+
+    //newLogo.render();
+    let shapeObj;
+    switch (shape) {
+        case "Circle":
+            shapeObj = new Circle(color);
+            break;
+        case "Triangle":
+            shapeObj = new Triangle(color);
+            break;
+        case "Ellipse":
+            shapeObj = new Ellipse(color);
+            break;
+        case "Rectangle":
+            shapeObj = new Rectangle(color);
+            break;
+
+        default:
+            throw new Error('Please enter a valid shape!');
+    }
+
+     newLogo.setText(fontColor, fontFamily, textInput);
+    newLogo.setShape(shape);
 
 };
 
 
 class Shape {
-    constructor(fillColor) {
-        this.fillColor = fillColor;
+    constructor(color) {
+        this.color = color;
     }
     render() {
         throw new Error('render() must be called by a subclass of shape.')
     }
 };
 
-class Text extends Shape {
-    constructor(fillColor, fontFamily, textInput){
-        super(fillColor);
-        this.fontFamily = fontFamily;
+class Text {
+    constructor(textInput, fontColor, fontFamily) {
         this.textInput = textInput;
+        this.fontColor = fontColor;
+        this.fontFamily = fontFamily;
+
     }
-    render() {
-        if(this.textInput.length > 3 && this.textInput.length < 1) {
+    renderText() {
+        if (this.textInput.length > 3 && this.textInput.length < 1) {
             throw new Error('textInput must be less than three characters and have at least one character.')
         }
-        return `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.fillColor}" font-family='${this.fontFamily}>${this.textInput}</text>`
+        return `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.fontColor}" font-family='${this.fontFamily}>${this.textInput}</text>`
     }
 };
 
 class Rectangle extends Shape {
-    constructor(fillColor) {
-        super(fillColor);
+    constructor(color) {
+        super(color);
     }
     render() {
-        `<rect x="150" y="100" rx="10" ry="10" width="200" height="200" fill="${this.fillColor}/>`
+        `<rect x="150" y="100" rx="10" ry="10" width="200" height="200" fill="${this.color}/>`
     }
 };
 
 class Circle extends Shape {
-    constructor(fillColor){
-        super(fillColor);
+    constructor(color) {
+        super(color);
     }
     render() {
-        return `<circle cx="150" cy="100" r="80" fill="${this.fillColor}"/>`
+        return `<circle cx="150" cy="100" r="80" fill="${this.color}"/>`
     }
 };
 
 class Triangle extends Shape {
-    constructor(fillColor){
-        super(fillColor);
+    constructor(color) {
+        super(color);
     }
     render() {
-        return `<polygon points="300 300, 150 0, 0 300" fill="${this.fillColor}"/>`
+        return `<polygon points="300 300, 150 0, 0 300" fill="${this.color}"/>`
     }
 }
 
 class Ellipse extends Shape {
-    constructor(fillColor){
-        super(fillColor)
+    constructor(color) {
+        super(color)
     }
     render() {
-        return `<ellipse cx="150" cy="100" rx="100" ry="60" fill="${this.fillColor}/>`
+        return `<ellipse cx="150" cy="100" rx="100" ry="60" fill="${this.color}/>`
     }
 }
+
+module.exports = generateSVG;
